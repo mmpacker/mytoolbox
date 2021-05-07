436d01e582409861b9d465c390cfc51cd8e15f7d
mmpacker <mmpacker@gmail.com>
2021-05-06 21:21:01 -0400

    add test landing page

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>MyToolbox Home<h1>')
+
+def landing(request):
+  return render(request, 'landing.html')
